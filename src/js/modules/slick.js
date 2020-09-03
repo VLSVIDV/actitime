f61efe2b70a96b11b14d6f3bcced2913b2fe9432
vlsvidv <vlsvidv@gmail.com>
2020-09-03 03:36:40 +0300

    add all mq for all sections

--- src/js/modules/slick.js
+++ src/js/modules/slick.js
@@ -1,7 +1,31 @@
 
 
 $(document).ready(function(){
-    $('.your-class').slick({
-      
+    $('.slick-slider').slick({
+        // variableWidth: true,
+        arrows: false,
+        dots: true,
+        responsive: [
+            {
+              breakpoint: 2500,
+              settings: {
+                slidesToShow: 2
+
+              }
+            },
+            {
+              breakpoint: 1920,
+              settings: {
+                slidesToShow: 1
+              }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+          ]
     });
 });
+
+
+
+
