dd5569ba1850dbdd03d2d243ebedfc9b3f05d4ab
vlsvidvmusic <vlsvidv@gmail.com>
2020-09-08 15:16:51 +0300

    add fixes n slider

--- src/js/modules/slick.js
+++ src/js/modules/slick.js
@@ -1,31 +1,19 @@
-
-
-$(document).ready(function(){
-    $('.slick-slider').slick({
-        // variableWidth: true,
-        arrows: false,
-        dots: true,
-        responsive: [
-            {
-              breakpoint: 2500,
-              settings: {
-                slidesToShow: 2
-
-              }
-            },
-            {
-              breakpoint: 1920,
-              settings: {
-                slidesToShow: 1
-              }
-            }
-            // You can unslick at a given breakpoint now by adding:
-            // settings: "unslick"
-            // instead of a settings object
-          ]
-    });
+$(document).ready(function () {
+  $(".slick-slider").slick({
+    // variableWidth: true,
+    arrows: false,
+    dots: true,
+    rows: 0,
+    mobileFirst: true,
+    slidesToShow: 1,
+    responsive: [
+      {
+        breakpoint: 1919,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+    ],
+  });
 });
-
-
-
-
